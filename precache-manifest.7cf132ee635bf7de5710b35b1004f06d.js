d6d86d81d6e2543d37696bc9e2bb797a1649b31f
maichKanyora <maichkanyora@localhost.localdomain>
2020-06-04 11:27:50 +0300

    Updates

---
+++ precache-manifest.7cf132ee635bf7de5710b35b1004f06d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f24270f88d46f0301acd7fbceb592f1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c305cd221fc7b030d6d0",
+    "url": "/monsters-rolodex/static/css/main.3c3c01ce.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c305cd221fc7b030d6d0",
+    "url": "/monsters-rolodex/static/js/main.e4bf105d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
